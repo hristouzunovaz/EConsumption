5bcc8385f7de7e25e0d069b716f1d6b96eb1ce4b
Hristo Uzunov <hristouzunovaz@gmail.com>
2020-02-04 21:23:06 +0200

    Initial Commit

---
+++ EConsumptionCalc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EConsumptionCalc
+//
+//  Created by Hristo Uzunov on 4.02.20.
+//  Copyright © 2020 Hristo Uzunov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
